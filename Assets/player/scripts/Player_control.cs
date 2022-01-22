d87ad4fdd1fb9c9e8be16e8bd792c3197b8343d5
omar <omarsalaheldinads@gmail.com>
2022-01-22 22:30:34 +0100

    player (omar andd sharawy parts)

---
+++ Assets/player/scripts/Player_control.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_control : MonoBehaviour
+{
+    private Animator animator;
+    public AudioSource FootSteps;
+    public AudioSource die;
+    public AudioSource hit;
+    public AudioSource soundOfTime;
+    public int maxHealth = 100;
+	public int currentHealth_player;
+    public HealthBar_player healthBar;
+    private bool playdie;
+    public GameObject boss;
+    public GameObject zombie;
+    private bool attackTrue;
+    public bool activated;
+    private int sandOfTime;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         animator = GetComponent<Animator>(); 
+         currentHealth_player = maxHealth;
+		 healthBar.SetMaxHealth(maxHealth);
+         playdie = false;
+         attackTrue = false;
+         activated = false;
+         sandOfTime = 0;
+    }
+
+    public void TakeDamage(int damage)
+	{
+		currentHealth_player -= damage;
+		healthBar.SetHealth(currentHealth_player);
+        if(currentHealth_player > 0){
+            hit.Play();
+        }
+        if(playdie == false){
+            if (currentHealth_player <= 0){
+            die.Play();
+			}
+        }
+        
+        
+	}
+    IEnumerator SandsOfTime()
+    {
+        Debug.Log("in enum");
+        activated = true;
+        Debug.Log(activated);
+        yield return new WaitForSeconds(5);
+        activated = false;
+        Debug.Log(activated);
+    }
+
+    void StartAbility()
+    {
+        Debug.Log("in method");
+        StartCoroutine(SandsOfTime());
+    }
+
+    private void died(){
+			if (currentHealth_player <= 0){
+            animator.SetTrigger("died");
+            playdie = true;
+			}
+		}
+        void attackbegins(){
+            attackTrue = true;
+        }
+
+        void attackends(){
+            attackTrue = false;
+        }
+       
+        void OnCollisionEnter(Collision collision){
+            if(collision.gameObject.tag == "boss"){
+                if (attackTrue){
+                    //    boss.TakeDamage(10);
+                    Destroy(collision.gameObject);
+                }
+            }
+             if(collision.gameObject.tag == "Zombie"){
+                if (attackTrue){
+                    //    zombie.TakeDamage(10);
+                }
+        }
+        if(collision.gameObject.tag == "sandsOfTime"){
+                Destroy(collision.gameObject);
+                sandOfTime+=1;
+        }
+        if(collision.gameObject.tag == "obstacle"){
+                // died();
+                // gameover
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+        died();
+
+        if (Input.GetKeyDown(KeyCode.W) | Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.S) | Input.GetKeyDown(KeyCode.A)
+		  | Input.GetKeyDown(KeyCode.LeftArrow) | Input.GetKeyDown(KeyCode.RightArrow) | Input.GetKeyDown(KeyCode.UpArrow) | Input.GetKeyDown(KeyCode.DownArrow)) {
+             if(!FootSteps.isPlaying) {
+                 FootSteps.Play();
+             }
+         }
+         if (Input.GetKeyUp(KeyCode.W) | Input.GetKeyUp(KeyCode.D) | Input.GetKeyUp(KeyCode.S) | Input.GetKeyUp(KeyCode.A)
+		  | Input.GetKeyUp(KeyCode.LeftArrow) | Input.GetKeyUp(KeyCode.RightArrow) | Input.GetKeyUp(KeyCode.UpArrow) | Input.GetKeyUp(KeyCode.DownArrow)) {
+             FootSteps.Stop();
+         }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            animator.SetTrigger("rolls");
+        
+		  if (Input.GetMouseButtonDown(0))
+          animator.SetTrigger("attacks");
+		  
+		  
+		  if (Input.GetMouseButtonDown(1))
+          animator.SetTrigger("defends");
+
+        //   if (Input.GetKeyDown(KeyCode.K)){
+        //     TakeDamage(10);
+        // }
+
+        if (Input.GetKeyDown(KeyCode.Q)){
+            if(sandOfTime > 0){
+                StartAbility();
+                soundOfTime.Play();
+                sandOfTime -=1;
+            }
+        }
+        
+        
+
+    }
+}
